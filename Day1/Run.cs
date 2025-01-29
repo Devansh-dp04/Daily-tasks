c5b5bf2c500d5322ec5b94fee30a8ffd0a78579f
devanshdp04 <devansh.patel@bacancy.com>
2025-01-29 12:24:29 +0530

    New folder uploaded

---
+++ Day1/Run.cs
@@ -0,0 +1,13 @@
+﻿using Day1;
+
+public class Run
+
+{
+    public static void Main()
+    {
+        Calculator calculator = new Calculator();
+        calculator.Calc();
+
+    }
+
+}
---
+++ Day2/CompressedString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    public  class CompressedString
+    {
+        public string Compress(string input)
+        {
+           
+            if (string.IsNullOrEmpty(input))
+                return "";
+
+            string result = "";
+            int count = 1;
+
+            
+            for (int i = 1; i < input.Length; i++)
+            {
+                
+                if (input[i] == input[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    
+                    result += input[i - 1];
+                    result += count.ToString();
+                    count = 1;
+                }
+            }
+
+            
+            result += input[input.Length - 1];
+            result += count.ToString();
+
+            // Return original string if compression doesn't save space
+            return result.Length < input.Length ? result : input;
+        }
+    }
+}
