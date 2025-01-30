a9f39938a8abdd27b34b4d6a23876ec2a983120c
devanshdp04 <devansh.patel@bacancy.com>
2025-01-30 15:10:14 +0530

    Updated files

---
+++ Day4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace day4 {
+    public class Programm
+    {
+
+       
+        public async Task UpdateVar1()
+        {
+            await Task.Delay(1000);//awaits execution for 1sec
+            Console.WriteLine("Executing Task1");
+            
+
+        }
+        public async Task UpdateVar2()
+        {
+            await Task.Delay(3000);//awaits execution for 3sec
+            
+            Console.WriteLine("Executing task2");
+            
+        }
+        public async Task UpdateVar3()
+        {
+            await Task.Delay(2000);//awaits execution for 2sec
+            
+            Console.WriteLine("Executing task3");
+
+        }
+        public async Task UpdateVar4()
+        {
+            await Task.Delay(5000);//awaits execution for 5sec
+            
+            Console.WriteLine("Executing task4");
+
+        }
+
+        public async Task TaskExecution(List<Task> tasks)
+        {
+
+            Task firstfinished = await Task.WhenAny(tasks.ToArray());
+            Console.WriteLine($"The first task completed");
+            tasks.Remove(firstfinished);
+            Console.WriteLine($"Executing remaining tasks");
+            int count = tasks.Count;
+            while (count > 0)
+            {
+                Task remaining =await Task.WhenAny(tasks.ToArray());
+                tasks.Remove(remaining);
+                count--;
+
+
+
+            }
+        }
+        
+
+    }
+
+}
+
