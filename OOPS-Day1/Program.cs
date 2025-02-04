2306b6c15d53c447fc2f0cf3cac5f84d38238347
devanshdp04 <devansh.patel@bacancy.com>
2025-02-04 14:41:30 +0530

    pushing the folder

---
+++ OOPS-Day1/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text;
+
+
+namespace OOPS_DAY1 {
+    public class Programm
+    {
+        public static void Main()
+        {
+
+            Student student1 = new Student( 1, "Devansh",  18, "A", 90);
+            Student student2 = new Student(2, "Harsh", 19, "B+", 75);
+            Student student3 = new Student(3, "Jemmy", 19, "C", 60);
+            Student student4 = new Student(4, "Dhirav", 21, "A+", 96);
+            Student student5 = new Student(5, "Utsav", 22, "A++", 99);
+
+            student1.ShowData(student1);
+            student2.ShowData(student2);
+            student3.ShowData(student3);
+            student4.ShowData(student4);
+            student5.ShowData(student5);
+
+            int Marks = 94;
+            //updating the student1 marks        
+            student1.Setmarks(Marks);
+            Console.WriteLine($"Updated {student1.Name}'s marks to {student1.GetMarks()}");
+
+            
+
+            //trying to input invalid marks
+            Marks = -12;
+            student1.Setmarks(Marks);
+
+            Console.Read();
+
+
+
+
+        }
+}
+
+}
+
+
+
---
+++ OOPS-Day1/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace OOPS_DAY1
+{
+    public class Student
+    {
+        public int Id;
+        public string Name;
+        public int Age;
+        public string Grade ;
+        private int Marks { get; set; }
+
+
+        public void ShowData(Student studentobj)
+        {
+            Console.WriteLine("ID: "+studentobj.Id);
+            Console.WriteLine("Name: "+studentobj.Name);
+            Console.WriteLine("Age: "+studentobj.Age);
+            Console.WriteLine("Grade: "+studentobj.Grade);
+            Console.WriteLine("Marks: "+studentobj.Marks);
+            Console.WriteLine("********************************************");
+
+        }
+        public void Setmarks(int marks) //Setter Accesor
+        {
+            //Marks validation
+            if (!(marks > 100 || marks < 0))
+            {
+                Marks = marks;
+            }
+            else
+            {
+                Console.WriteLine($"{marks} is not a valid mark");
+    
+        }
+        }
+
+        public int GetMarks() //Getter Accesor
+        {
+            return Marks;
+        }
+
+        //values initialized
+        public Student(int id, string name, int age, string grade,int marks)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Grade = grade;
+            Marks = marks;
+        }
+
+        //Destructor
+        ~Student()
+        {
+            Console.WriteLine("Object Destroyed.Space cleaned up.");
+        }
+
+
+
+    }
+
+}
+
